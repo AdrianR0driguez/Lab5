d51fc2d19023cccda173a0b020d8bb2847c2f7af
AdrianR0driguez <007923676@coyote.csusb.edu>
2024-03-20 23:57:00 -0700

    Update

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "RPG.h"
+
+using namespace std;
+
+int main(){
+    RPG defaultConstructor;
+    RPG overloadedConstructor();
+    
+    cout << 
+}
+
+/*
+Remaining functions:
+void attack(RPG *);
+void useSkill(RPG *);
+void printAction(string, RPG);
+*/
